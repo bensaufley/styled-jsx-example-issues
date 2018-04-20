f8dd407e3c7b6fb4560f80b4555a85e89c8afdba
Ben Saufley <bensaufley+git@gmail.com>
2018-04-20 15:43:30 -0400

    Enable sourceMaps.
    No indexOf on undefined filename.

--- test/support/babel-register.js
+++ test/support/babel-register.js
@@ -7,6 +7,4 @@ const babelConfig = {
   extensions: ['.js', '.jsx', '.ts', '.tsx'],
 };
 
-console.log('babel config:', JSON.stringify(babelConfig, null, 2));
-
 require('@babel/register')(babelConfig);
