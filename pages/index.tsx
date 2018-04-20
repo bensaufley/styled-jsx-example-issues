e275a9c1d673e48d129f1a59a98320432376b901
Ben Saufley <bensaufley+git@gmail.com>
2018-04-20 14:11:26 -0400

    Re-apply styles.
    npm test produces "Cannot read property 'indexOf' of undefined" in source-map/lib/util.js:206

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
-// import styles from './styles';
+import styles from './styles';
 
 export default () => (
   <h1>
     Test
-    {/* <style jsx>{styles}</style> */}
+    <style jsx>{styles}</style>
   </h1>
 );
